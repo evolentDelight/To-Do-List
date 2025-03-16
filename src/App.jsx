c9e013db0be52d0ece3a05c3d44effa92e0b888a
evolentDelight <evolentDelight@outlook.com>
2025-03-16 11:33:11 -0700

    Base CSS design

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [ToDoList, setList] = useState('[]')
+  const [ToDoList, setTDList] = useState([])
 
   return (
-    <>
-      
-    </>
+    <div className="list-main">
+      <h1 className="list-title">To Do List</h1>
+      <hr />
+      <div className="list-actual-list">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
