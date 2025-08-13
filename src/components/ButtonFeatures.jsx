e186a6508ccb0ac0922a3d190090b509b7836eeb
evolentDelight <evolentDelight@outlook.com>
2025-08-13 09:10:15 -0700

    Minor edit: Change button label

--- src/components/ButtonFeatures.jsx
+++ src/components/ButtonFeatures.jsx
@@ -4,7 +4,7 @@ export default function ButtonFeatures({ addNewTask, deleteAllTasks }) {
   return (
     <div className="button-features-container">
       <button className="bfbutton" id="add-new-to-do" onClick={addNewTask}>
-        New To Do
+        Add To Do
       </button>
       <button className="bfbutton" id="delete-all" onClick={deleteAllTasks}>
         Delete All
